fac18bf3076cba6526e2a14645e122b552137fd5
liuzhen <liuzhen1010xyz@gmail.com>
2020-10-31 16:04:14 +0800

    jsx的标签解析

--- week13/jsx/main.js
+++ week13/jsx/main.js
@@ -1,5 +1,72 @@
-for(let i of [1, 2, 3, 4]) {
-    console.log(i);
+function createElement(type, attributes, ...children) {
+    let element = null;
+    // 鸭子类型：无论element是鸭子（dom）还是狗子（组件），都调用相关方法
+    if (typeof type === 'string') {
+        element = new ElementWrapper(type);
+    } else {
+        element = new type;
+    }
+    
+    for (const name in attributes) {
+        element.setAttribute(name, attributes[name]);
+    }
+    // children会自动编译为 createElement(child)
+    for (const child of children) {
+        if (typeof child === 'string') {
+            child = new TextWrapper(child);
+        }
+        element.append(child);
+    }
+    return element;
+}
+class TextWrapper {
+    constructor(content) {
+        this.root = document.createTextNode(content);
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+    append(child) {
+        child.montTo(this.root);
+    }
+    montTo(parent) {
+        parent.append(this.root);
+    }
+}
+class ElementWrapper {
+    constructor(type) {
+        this.root = document.createElement(type);
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+    append(child) {
+        child.montTo(this.root);
+    }
+    montTo(parent) {
+        parent.append(this.root);
+    }
 }
 
-let a = <div/>
+class Div {
+    constructor() {
+        this.root = document.createElement('div');
+    }
+    setAttribute(name, value) {
+        this.root.setAttribute(name, value);
+    }
+    append(child) {
+        child.montTo(this.root);
+    }
+    montTo(parent) {
+        parent.append(this.root);
+    }
+}
+
+let a = <Div id="a">
+    <span>d</span>
+    <span>c</span>
+</Div>
+// 这里 组件的a.root才是dom元素，document.body.append(a.root)才可以
+// 但是又不能在DiV内设置this，所以使用另一种办法a.montTo
+a.montTo(document.body);
