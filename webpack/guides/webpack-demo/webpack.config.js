8425cded8863165f15eb4d2a58c1c68e59c8b93c
liuzhen <liuzhen1010xyz@gmail.com>
2021-01-04 17:05:32 +0800

    4.加载 images 图像

--- webpack/guides/webpack-demo/webpack.config.js
+++ webpack/guides/webpack-demo/webpack.config.js
@@ -2,7 +2,18 @@ const path = require('path');
 module.exports = {
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [// 要保证顺序
+                    'style-loader',
+                    'css-loader',
+                ]
+            }
+        ]
     }
 }
