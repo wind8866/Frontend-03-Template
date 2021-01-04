40be74ab89521d3749a2fdc9e3fbb7a8dcc6e019
刘振 <liuzhen@liuzhendeiMac.local>
2021-01-04 21:44:17 +0800

    11.防止重复

--- webpack/guides/webpack-demo/webpack.config.js
+++ webpack/guides/webpack-demo/webpack.config.js
@@ -1,23 +1,11 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     entry: {
         app: './src/index.js',
-        print: './src/print.js',
+        another: './src/another-module.js',
     },
-    devtool: 'inline-source-map',
-    devServer: {
-        contentBase: './dist',
-    },
-    plugins: [
-        new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-        new HtmlWebpackPlugin({
-            title: 'Development',
-        }),
-    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
